bb7a529970199ce65626690c20eab5717bff72dc
shaheedhudheen <shaheedhudheenpambalath@gmail.com>
2021-10-11 00:03:21 +0530

    heroku added

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Activate Django-Heroku
+
+django_heroku.settings(locals())
